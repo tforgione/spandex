8eb02d7fce6d97639ff195f56edead02022e5c53
Cedd Burge <ceddlyburge@gmail.com>
2021-03-29 09:56:49 +0100

    Add `level` to UnorderedListItem
    This isn't parsed / used for the output yet, and is currently always 0

--- src/parser/combinators.rs
+++ src/parser/combinators.rs
@@ -245,9 +245,10 @@ pub fn parse_title(input: Span) -> IResult<Span, Ast> {
 /// let list = parse_unordered_list(input).unwrap().1;
 /// assert_eq!(list,
 ///     Ast::UnorderedList(
-///         vec![Ast::UnorderedListItem(
-///             vec![Ast::Text(String::from("This is my list"))]
-///         )]
+///         vec![Ast::UnorderedListItem { 
+///             level: 0, 
+///             children: vec![Ast::Text(String::from("This is my list"))]
+///         }]
 ///     )
 /// );
 /// ```
@@ -267,9 +268,10 @@ pub fn parse_unordered_list(input: Span) -> IResult<Span, Ast> {
 /// let input = Span::new("- This is my list");
 /// let item = parse_unordered_list_item(input).unwrap().1;
 /// assert_eq!(item,
-///     Ast::UnorderedListItem(
-///         vec![Ast::Text(String::from("This is my list"))]
-///     )
+///     Ast::UnorderedListItem {
+///         level: 0,
+///         children: vec![Ast::Text(String::from("This is my list"))]
+///     }
 /// );
 /// ```
 pub fn parse_unordered_list_item(input: Span) -> IResult<Span, Ast> {
@@ -294,7 +296,15 @@ pub fn parse_unordered_list_item(input: Span) -> IResult<Span, Ast> {
 
     let (_, content) = parse_group(content)?;
 
-    Ok((input, Ast::UnorderedListItem(content)))
+    Ok(
+        (
+            input, 
+            Ast::UnorderedListItem {
+                level: 0,
+                children: content
+            }
+        )
+    )
 }
 
 ////////////////////////////////////////////////////////////////////////////////
